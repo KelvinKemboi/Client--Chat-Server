594641429545d9ee05ff8486460fc19ebdf1c43f
kkemboi <kemboikelvin185@gmail.com>
2026-05-04 20:13:24 -0400

    Created basic TCP server

---
+++ src/server.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+using namespace std;
+
+int main(){
+    //create socket
+    int sock=socket(AF_INET, SOCK_STREAM, 0);
+    
+    //address
+    int port=8000;
+    sockaddr_in addr={};
+    addr.sin_family=AF_INET;
+    addr.sin_port=htons(port);
+    addr.sin_addr.s_addr=INADDR_ANY;
+
+    //bind
+    bind(sock, (sockaddr*)&addr, sizeof(addr));
+    listen(sock, 20);
+
+    //print server
+    cout<<"Server is listening on port: "<<port<<endl;
+
+    //accept connection
+    while(true){
+        int client=accept(sock, nullprt, nullprt);
+        cout<<"Client connected: "<<client<<endl;
+
+        
+
+        close(client);
+    }
+
+
+    return 0;
+}
